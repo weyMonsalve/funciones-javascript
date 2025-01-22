99c86f8d74d15ab037ec0c7afdc360d9e92c3cb2
wbeimar <wbeimarmonsalve33@gmail.com>
2025-01-22 17:54:19 -0500

    Numeros equivalentes a numaro romano

--- index.js
+++ index.js
@@ -7,17 +7,34 @@
 // }
 
 
-//   Leer tres números y decir cuál de ellos es el que tiene el valor intermedio
+//   Leer un número entre el 1 y el 10 e imprimir su equivalente en números romanos
 
-function numeroIntermedio(num1, num2, num3) {
-    if ((num1 < num2 && num1 > num3) || (num1 > num2 && num1 < num3)) {
-        return "El numero intermedio es el: " + num1;
-    } else if ((num2 > num1 && num2 < num3) || (num2 < num1 && num2 > num3)) {
-        return "El numero intermedio es el: " + num2;
-    } else {
-        return "El numero intermedio es el: " + num3;
+function numeroRomano(numero) {
+    switch (numero) {
+        case 1:
+            return "El numero romano de 1 es I";
+        case 2:
+            return "El numero romano de 2 es II";
+        case 3:
+            return "El numero romano de 3 es III";
+        case 4:
+            return "El numero romano de 4 es IV";
+        case 5:
+            return "El numero romano de 5 es V";
+        case 6:
+            return "El numero romano de 6 es VI";
+        case 7:
+            return "El numero romano de 7 es VII";
+        case 8:
+            return "El numero romano de 8 es VIII";
+        case 9:
+            return "El numero romano de 9 es IX";
+        case 10:
+            return "El numero romano de 10 es X";
+        default:
+            return "El numero no es valido";
     }
 }
 
-let intermedio = numeroIntermedio(1, 5, 3);
-console.log(intermedio);
+let romano = numeroRomano(8);
+console.log(romano);
