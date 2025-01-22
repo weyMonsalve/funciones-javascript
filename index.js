2036f3539a311d21fa7eed356483bfda7abcd7e7
wbeimar <wbeimarmonsalve33@gmail.com>
2025-01-22 17:59:59 -0500

    Sacando el promedio de calificaciones

--- index.js
+++ index.js
@@ -7,34 +7,12 @@
 // }
 
 
-//   Leer un número entre el 1 y el 10 e imprimir su equivalente en números romanos
+//   Leer tres calificaciones de un alumno y determinar su promedio
 
-function numeroRomano(numero) {
-    switch (numero) {
-        case 1:
-            return "El numero romano de 1 es I";
-        case 2:
-            return "El numero romano de 2 es II";
-        case 3:
-            return "El numero romano de 3 es III";
-        case 4:
-            return "El numero romano de 4 es IV";
-        case 5:
-            return "El numero romano de 5 es V";
-        case 6:
-            return "El numero romano de 6 es VI";
-        case 7:
-            return "El numero romano de 7 es VII";
-        case 8:
-            return "El numero romano de 8 es VIII";
-        case 9:
-            return "El numero romano de 9 es IX";
-        case 10:
-            return "El numero romano de 10 es X";
-        default:
-            return "El numero no es valido";
-    }
+function promedioNotas(cal1, cal2, cal3) {
+    let calificacionPromedio = ((cal1 + cal2 + cal3) / 3).toFixed(2);
+    return "El promedio de las notas es: " + calificacionPromedio;
 }
 
-let romano = numeroRomano(8);
-console.log(romano);
+let promedio = promedioNotas(4, 5, 4, 2, 5, 0);
+console.log(promedio);
