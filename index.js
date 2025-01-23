8a32de053677a4d7ce956eac9d6900306e54f077
wbeimar <wbeimarmonsalve33@gmail.com>
2025-01-22 19:10:14 -0500

    calculadora2

--- index.js
+++ index.js
@@ -12,29 +12,23 @@
 //  al digitar dos números.
 
 
-function calculador(num1, num2) {
-    return function(operador) {
-        if (operador === 'suma') {
-            return num1 + num2;
+function calculador2(num1, num2) {
+    return function(operator) {
+        switch (operator) {
+            case 'suma':
+                return num1 + num2;
+            case 'resta':
+                return num1 - num2;
+            case 'multiplicacion':
+                return num1 * num2;
+            case 'division':
+                return num1 / num2;
         }
-        if (operador === 'resta') {
-            return num1 - num2;
-        }
-        if (operador === 'multiplicacion') {
-            return num1 * num2;
-        }
-        if (operador === 'division') {
-            if (num2 === 0) {
-                return 'Error: No se puede dividir entre 0';
-            }
-            return num1 / num2;
-        }
-        return 'Error: Operador no válido';
     };
 }
 
 
-var calculo = calculador(75, 50);
+var calculo = calculador2(75, 50);
 var sum = calculo('suma');
 var rest = calculo('resta');
 var mult = calculo('multiplicacion');
