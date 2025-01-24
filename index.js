f52fe655fe8f422fc61fd1344d9efb5d7dce78ef
wbeimar <wbeimarmonsalve33@gmail.com>
2025-01-24 10:07:25 -0500

    Conversion de horas a minutos con validacion y archivo de importacion

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 // Promedio de tres números
 
-// Función calcularPromedio
-function calcularPromedio(num1, num2, num3) {
+// Convertir horas y minutos a segundos
 
-    if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
-        return "Los valores deben ser numericos."
+// Función que convierte horas y minutos a segundos
+function convertirAHorasASegundos(horas, minutos) {
+
+    if (isNaN(horas) || isNaN(minutos)) {
+        return "Los datos ingresados deben ser numericos.";
     }
 
-    let suma = num1 + num2 + num3;
+    // Convertimos las horas a segundos (1 hora = 3600 segundos)
+    const segundosPorHoras = horas * 3600;
 
-    let promedio = suma / 3;
+    // Convertimos los minutos a segundos (1 minuto = 60 segundos)
+    const segundosPorMinutos = minutos * 60;
 
-    return promedio;
+    // Devolvemos el total de segundos
+    return segundosPorHoras + segundosPorMinutos;
 }
 
 
-
 module.exports = {
-    calcularPromedio
+    convertirAHorasASegundos
 };
