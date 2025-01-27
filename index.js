c762b7e97163519f719d293cf345f4e38a0193f8
wbeimar <wbeimarmonsalve33@gmail.com>
2025-01-27 10:32:35 -0500

    Suma de los numeros de un Array con Arrow functions

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-//   Convertir una temperatura de Celsius a Fahrenheit usando una arrow function.
+//   Crear una arrow function que reciba un array de números y devuelva la suma de todos los elementos.
 
-const convertirAFahrenheit = (celsius) => {
-    return (celsius * 9 / 5) + 32;
-}
+
+// index.js
+
+
+const sumarArray = (numeros) => {
+    let suma = 0;
+
+
+    numeros.forEach((numero) => {
+        suma += numero;
+    });
+
+    return suma;
+};
+
 
 module.exports = {
-    convertirAFahrenheit
-}
+    sumarArray
+};
